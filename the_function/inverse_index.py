f5e857d243b919ae081df7f27462eb1dd7a0795a
George Camboim <george.camboim@embedded.ufcg.edu.br>
2020-08-19 06:42:34 -0300

    Made the 'orSearch' and 'andSearch' for inverse indexes

--- the_function/inverse_index.py
+++ the_function/inverse_index.py
@@ -1,7 +1,22 @@
 def main():
     f = open('resources/stories_big.txt')
     strlist = list(f)
-    print(makeInverseIndex(strlist))
+    inverseIndex = makeInverseIndex(strlist)
+    while True:
+        resp = input("Do you want to do a orSearch or a andSearch? (x to quit) ").lower()
+        if resp == 'x':
+            print("Thank You!")
+            break
+        elif resp != 'or' and resp != 'orsearch' and resp != 'and' and resp != 'andsearch':
+            print("I didn't understand you, asking again...")
+            continue
+        querry = input("Enter your search querry: ").split()
+        if resp == 'or' or resp == 'orsearch':
+            search_set = orSearch(inverseIndex, querry)
+        else:
+            search_set = andSearch(inverseIndex, querry)
+        print("The documents where you can find what you want are...")
+        print(search_set)
 
 def makeInverseIndex(strlist):
     doc_word_set = []
@@ -15,6 +30,22 @@ def makeInverseIndex(strlist):
             else:
                 inv_index[word] = {i+1}
     return inv_index
+
+def orSearch(inverseIndex, querry):
+    search_word_arr = []
+    for word in querry:
+        if word in inverseIndex:
+            search_word_arr.append(inverseIndex[word])
+    return set.union(*search_word_arr)
+
+def andSearch(inverseIndex, querry):
+    search_word_arr = []
+    for word in querry:
+        if word in inverseIndex:
+            search_word_arr.append(inverseIndex[word])
+        else:
+            print("WARNING: '%s' doesn't seems to exist in dict. Ignoring it." %(word))
+    return set.intersection(*search_word_arr)
 
 if __name__ == '__main__':
     main()
